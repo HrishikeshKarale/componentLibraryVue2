f67c6b2fb0d4965b540c58e003af812e0e15500b
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2021-01-07 06:40:08 -0500

    added assets folder for vue-image default image source

--- src/components/Layout/enterpriseAppLayout.vue
+++ src/components/Layout/enterpriseAppLayout.vue
@@ -20,18 +20,9 @@
           <scroll-indicator>
             <router-view :key="$route.path" />
           </scroll-indicator>
-          <aside class="moto">
-            <vue-img :src="logo" alt="Moto" />
-            <q>
-              A little
-              <abbr
-                title="The action of understanding, being aware of, being sensitive to, and vicariously experiencing the feelings, thoughts, and experience of another of either the past or present without having the feelings, thoughts, and experience fully communicated in an objectively explicit manner"
-              >
-                Empathy
-              </abbr>
-              goes a long way
-            </q>
-          </aside>
+          <template v-if="$slots['moto']">
+            <slot name="moto" />
+          </template>
         </div>
       </transition>
     </div>
@@ -42,10 +33,8 @@
 </template>
 
 <script>
-import { nav } from "@/store/navigation";
 import scrollIndicator from "@/components/UIComponents/Navigation/scrollIndicator.vue";
 import breadcrums from "@/components/UIComponents/Navigation/breadcrums";
-import vueImg from "../UIComponents/Image/vueImg.vue";
 // import { authentication } from "@/typeScript/authentication";
 import { cookie } from "@/typeScript/cookie.js";
 
@@ -53,8 +42,15 @@ export default {
   name: "EnterpriseAppLayout",
   components: {
     scrollIndicator,
-    breadcrums,
-    vueImg
+    breadcrums
+  },
+
+  props: {
+    navigation: {
+      type: [Object, Array],
+      required: true,
+      default: null
+    }
   },
 
   mixins: [/*authentication, */ cookie],
@@ -65,14 +61,12 @@ export default {
     const DEFAULT_TRANSITION_MODE = "out-in";
     const transitionEnterActiveClass = "";
     const prevHeight = 0;
-    const navigation = nav;
     return {
       logo,
       transitionName: DEFAULT_TRANSITION,
       transitionMode: DEFAULT_TRANSITION_MODE,
       transitionEnterActiveClass,
-      prevHeight,
-      navigation
+      prevHeight
     };
   }, //mixins
 
@@ -212,34 +206,6 @@ export default {
           margin: 0 auto;
           max-width: 80vw;
           width: 1504px;
-
-          & > .moto {
-            display: flex;
-            flex-direction: column;
-            position: absolute;
-            bottom: 0;
-            right: 0;
-            padding: @spaceMd @spaceLg;
-            border-radius: @borderRadius 0 0 0;
-            // background-color: @backgroundColor;
-            height: fit-content;
-
-            & > img {
-              height: 48px;
-            }
-
-            & > q {
-              font-size: @fontSizeSm;
-              font-weight: bold;
-              // background-color: @backgroundColor;
-              border-radius: @borderRadius 0 0 0;
-
-              & > abbr {
-                color: @primaryColor;
-                text-decoration: none;
-              }
-            }
-          }
         }
       }
     }
@@ -250,16 +216,6 @@ export default {
         &.body {
           & > div.content {
             min-width: 480px;
-            & > .moto {
-              padding: 0;
-
-              & > img {
-                display: none;
-              }
-              & > q {
-                padding: @spaceSm @spaceMd;
-              }
-            }
           }
         }
       }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 //formInputs
-export { default as vueText } from "./components/FormElements/inputInput.vue";
+export { default as vueText } from "./components/FormElements/textInput.vue";
 export { default as vuePassword } from "./components/FormElements/passwordInput.vue";
 export { default as vueCheckbox } from "./components/FormElements/checkboxInput.vue";
 export { default as vueEmail } from "./components/FormElements/emailInput.vue";
@@ -16,7 +16,7 @@ export { default as vueTextarea } from "./components/FormElements/vueTextarea.vu
 //alerts
 export { default as vueInputResponse } from "./components/Alerts/inputResponse.vue";
 export { default as vueAlert } from "./components/Alerts/vueAlert.vue";
-export { default as vuePageNotFound } from "./components/FormElements/Input.vue./components/Alerts/systemError.vue";
+export { default as vuePageNotFound } from "./components/Alerts/systemError.vue";
 
 //code
 export { default as vueClipboard } from "./components/Code/vueClipboard.vue";
@@ -24,17 +24,16 @@ export { default as vueClipboard } from "./components/Code/vueClipboard.vue";
 //layout
 export { default as enterpriseAppLayout } from "./components/Layout/enterpriseAppLayout.vue";
 export { default as goldenRatioLayout } from "./components/Layout/goldenRatioLayout.vue";
-export { default as packStationLayout } from "./components/Layout/packStationLayout.vue";
 export { default as sideBySideLayout } from "./components/Layout/sideBySideLayout.vue";
 
 //UIComponents
 export { default as vueButton } from "./components/UIComponents/Button/index.vue";
 export { default as vueFilter } from "./components/UIComponents/Filter/vueFilter.vue";
-export { default as vueImage } from "./components/UIComponents/Image/vueImage.vue";
+export { default as vueImage } from "./components/UIComponents/Image/vueImg.vue";
 export { default as vueModal } from "./components/UIComponents/Modal/vueModal.vue";
 export { default as vueHeader } from "./components/UIComponents/Navigation/vueHeader.vue";
 export { default as vueSideNav } from "./components/UIComponents/Navigation/sideNav.vue";
 export { default as vueScrollIndicator } from "./components/UIComponents/Navigation/scrollIndicator.vue";
 export { default as vueBreadcrum } from "./components/UIComponents/Navigation/breadcrums.vue";
 export { default as vueWizard } from "./components/UIComponents/StepWizard/vueWizard.vue";
-export { default as vueTable } from "./components/UIComponents/Table/vueTable.vue";
+// export { default as vueTable } from "./components/UIComponents/Table/vueTable.vue";
